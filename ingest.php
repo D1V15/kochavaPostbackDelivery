8487e2456c321fcd9026d8bb33240065595b6dcb
D1V15 <antondivis@gmail.com>
2016-03-01 22:52:27 -0800

    Added Class and created Post Back Object
    I added the postbackObject class and also created an array that represents what the POST body will look like once the JSON is decoded. I then parsed out the array and created a PostbackObject. This object was then added to Redis and then retrieved.

--- ingest.php
+++ ingest.php
@@ -1,24 +1,49 @@
 <?php
 require "/predis/autoload.php";
-echo "passed require\n\n";
 Predis\Autoloader::register();
-echo "Hello There\n\n";
 // since we connect to default setting localhost
 // and 6379 port there is no need for extra
 // configuration. If not then you can specify the
 // scheme, host and port to connect as an array
 // to the constructor.
+class postbackObject {
+    public $method = "";
+    public $url  = "";
+    public $key = "";
+    public $value = "";
+}
+$postback = array(endpoint=> array(method=>"GET", url=>"http://sample_domain_endpoint.com/data?key={key}&value={value}&foo={bar}"), data=> array(key=>"Azureus", value=>"Dendrobates"));
+//v = file_get_contents('php://input');
+//$inputJSON = json_decode($entityBody);
+$method = $postback["endpoint"]["method"];
+$url = $postback["endpoint"]["url"];
+$data = $postback["data"];
+
+
+echo "</br>";
 try {
-	echo "connect to redis";
     $redis = new Predis\Client();
     echo "Successfully connected to Redis";
-$redis->set("I 2 love Php!", "Also Redis now!");
-$value = $redis->get("I 2 love Php!");
-echo $value;
+    echo "</br>";
+    echo "</br>";
+    //for ($x = 0; $x <= 10; $x++) {
+    $newObject = new postbackObject();
+    $newObject->method = $method;
+    $newObject->url = $url;
+    $newObject->key = $data["key"];
+    $newObject->value = $data["value"];
+    $temp = json_encode($newObject);
+    $redis->rPush("postbackObject", $temp);
+    //}
+
+    echo $redis->lPop("postbackObject");
+    echo "</br>";
 }
 catch (Exception $e) {
     echo "Couldn't connected to Redis";
+    echo "</br>";
     echo $e->getMessage();
+    echo "</br>";
 }
-echo "what the heck";
+
 ?>
